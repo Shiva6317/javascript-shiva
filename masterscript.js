23b98000443511dd6442691281b0e596cf9c1f3e
Sanjay Mishra <shivjeetpaswan86@gmail.com>
2023-09-13 15:38:47 +0530

    master in javascirpt initial commit

---
+++ masterscript.js
@@ -0,0 +1,104 @@
+// ///////////////////✅✅✅variable var vs let✅✅✅////////////
+// function hero(){
+
+
+//     // for(var i=0;i<10;i++){
+//     //     console.log(i);
+//     // }
+//     // console.log(i);//code show no error
+
+    
+//     for(let i=0;i<10;i++){
+//         console.log(i);
+//     }
+//     console.log(i);
+// }
+
+// hero()
+
+//HOW TO COPY REFERENCE VALUE✅✅✅✅✅✅✅✅
+// let hero=[1,2,3,4,5];
+
+// // let a=hero; without derefrensing value
+
+// let a=[...hero]; //with derefrencing
+
+// a.pop()
+// console.log(hero);
+
+
+
+// truthy and falsy       ✅✅✅✅✅✅✅✅✅✅✅
+// falsy value=======>>>>>>[🆕🆕0,NaN,undefined,false ,null,document.all🆕🆕]
+// truthy value==========>>>>>all the value eccept falsy known as truthy
+
+// if(2-2){
+//     console.log("truthy")
+// }else{
+//     console.log("falsy")
+// }
+
+
+////////////////////// ✅✅✅✅✅Master all loops in javascritp✅✅✅✅✅/////////
+// forEach only working on Array 🆕🆕🆕🆕
+
+// var hero=[1,2,3,4,5]
+
+// hero.forEach(element => {
+//     console.log(element);
+// });
+
+////////✅✅✅✅✅forin  loop working on object✅✅✅✅✅
+
+// var  obj1={
+//     name:"shiva",
+//     educaton:"B.tech",
+//     age:18
+// }
+// let obj={...obj1};
+
+// for (var key in obj) {
+//     obj[key]="shiva"
+//     console.log(key,"->",obj[key]);
+
+// }
+
+// for (var key in obj1) {
+//     obj[key]="shiva"
+//     console.log(key,"->",obj1[key]);
+
+// }
+
+////////////✅✅✅✅✅✅callback function in javascript
+
+// setTimeout,setInterval these are async javascript
+
+
+//////////✅✅✅✅✅Frist class function✅✅✅✅✅✅✅✅//////////
+
+// function shiva(a){
+//     a();
+
+// }
+
+// shiva(function(){console.log(shiva)})
+
+///////////✅✅✅✅✅✅//local variablejay hind jay bharat ka hall haibrother
+
+// var a={
+//     name:"sshiva",
+//     age:34
+// }
+
+// delete a.age;
+// delete a.name;
+
+
+// console.log(a);
+
+
+
+
+
+
+
---
+++ script.js
@@ -0,0 +1,22 @@
+// word vs keyword🆗✅
+// chacha =word
+// for=keyword
+/////////////////////////////////////////////////////////
+
+//var vs const vs let
+// var hero
+// let hero
+// let hero
+
+///////////////////////////////////////////////////////////
+
+
+// Hoisting✔️✅
+
+// console.log(a);
+// var a=12;
+// console.log(b);
+
+// premitive vs reference✅
+
+// var a=2389;premitive; var a={},[],() refenrence;🔜
