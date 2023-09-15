27cc6bd03aed76d177d0ab1d2655258686ce90b2
Sanjay Mishra <shivjeetpaswan86@gmail.com>
2023-09-15 14:28:09 +0530

    some react concept

---
+++ asyncjs.js
@@ -0,0 +1,41 @@
+//////////////♾️♾️asynchronous javascript♾️♾️///////////////////
+
+
+// sync=======>{line by line ✅✅✅}javascript is a line by line code runner
+///async========>{yak sath sab chalta hai}
+
+// these are async javascript example
+// fetch
+// XMLHttpRequest
+// axios
+// Promise
+// setInterval
+// setTimeout
+/////////////////////////////////////////////////////////////////////
+// jab inka ans aa jaye to ye function chal jaye example
+// useCallback(
+//   () => {
+//     first
+//   },
+//   [second],
+// )
+// async await
+// then catch
+
+
+
+///////////////////////////✅✅REACT SOME CONCEPT✅✅////////////////////////
+
+// real dom and vertual dom in react 
+// virtual dom represented by react
+
+// in real dom when we update any component then whole dom tree updated
+//but in virtual dom that thing not happen react component update all the feature of website
+
+
+
+
+
+
+
+
